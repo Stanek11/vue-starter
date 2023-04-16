b8d4ce416da762273d6e4e308841bf601556ed81
pstankie <pstankie@d17-430-pc-08.ipa.ii.agh.edu.pl>
2023-04-16 14:50:19 +0200

    miligram

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="my-app">
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
-    <h1>Twój e-mail to {{ email }}</h1>
+    <div v-if="loggedin">Zalogowany  {{email}}
+      <a @click="logMeOut()">Wyloguj</a>
+    </div>
+    <div v-else>
     <input type="email" v-model="email">
+      <button @click="logMeIn()">Zaloguj się</button>
     <div v-if="!email.includes('@')">Twój mail jest nieprawidłowy</div>
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    {{email.length}}
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
-    
+
+    </div>
   </div>
 </template>
 
 <script>
+
+import "milligram";
 export default {
 data() {
   return {
     email: 'patryk.stankiewicz@gmail.com',
+    loggedin: false,
     password: ''
-
   };
 },
   methods: {
-    alertMyEmail() {
-      alert(this.email);
-    }
+  logMeIn() {
+    this.loggedin = true;
+  },
+    logMeOut() {
+      this.loggedin = false;
+    },
   }
-
 }
+
 </script>
 
 <style>
